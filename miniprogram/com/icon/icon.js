141230a64f7e9990dda9322630b8471a0c585b61
ccy <957036264@qq.com>
2019-07-11 09:28:22 +0800

    ccy1
    7/10/9

---
+++ miniprogram/com/icon/icon.js
@@ -0,0 +1,30 @@
+// com/icon/icon.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    icon: {
+      type: String,
+      value: ''
+    },
+    ictext: {
+      type: String,
+      value: ''
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
